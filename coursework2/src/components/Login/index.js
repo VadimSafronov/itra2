c292813b7fbe9ad9569ccc15e246525aca7c4e97
Vadim Safronov <vadimisprice@tut.by>
2020-05-29 01:34:12 +0300

    fix button

--- coursework2/src/components/Login/index.js
+++ coursework2/src/components/Login/index.js
@@ -9,6 +9,27 @@ import GoogleAuth from './GoogleAuth'
 
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        height: 'calc(100vh - 64px)',
+        minHeight: 390,
+        marginTop: 64,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        [theme.breakpoints.down('sm')]: {
+            height: 'calc(100vh - 128px)',
+            marginTop: 113,
+        },
+    },
+    loginContainer: {
+        width: 362,
+        display: 'flex',
+        flexDirection: 'column',
+        [theme.breakpoints.down('xs')]: {
+            width: '100%',
+            padding: '0 30px',
+        },
+    },
 socialGroup: {
     marginBottom: 31,
     display: 'flex',
@@ -21,8 +42,8 @@ const Login = (props) => {
 
     return (
         
-            <div>
-            
+        <div className={classes.root}>
+        <div className={classes.loginContainer}>
 
                 
 
@@ -33,6 +54,7 @@ const Login = (props) => {
                 </div>
                 <SignIn onSubmit={props.login} />
                 </div>
+        </div>
         
         
     )
