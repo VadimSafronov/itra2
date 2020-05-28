5fa6476ca2df896c0526884b064666d818b396b0
Vadim Safronov <vadimisprice@tut.by>
2020-05-29 01:48:37 +0300

    facebook auth

---
+++ coursework2/src/components/Login/FacebookAuth/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import FacebookButton from './FacebookButton'
+import { connect } from 'react-redux'
+import { socialLogin } from '../../../Redux/authReducer'
+
+const FacebookAuth = (props) => {
+    const responseFacebook = (response) => {
+        const socialId = response.id
+        const name = response.name
+        props.socialLogin(socialId, name)
+    }
+
+    return (
+        <FacebookLogin
+            appId='271109940744192'
+            fields='name'
+            callback={responseFacebook}
+            render={(renderProps) => <FacebookButton {...renderProps} />}
+        />
+    )
+}
+export default connect(null, { socialLogin })(FacebookAuth)
