a99df056efd0893f408923626846e3fc00d283e8
Vadim Safronov <vadimisprice@tut.by>
2020-05-28 17:44:44 +0300

    redirect profile

--- coursework2/src/components/Profile/index.js
+++ coursework2/src/components/Profile/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { withLoginRedirect } from '../../hoc/withAuthRedirect'
-import { compose } from 'redux'
-import { connect } from 'react-redux'
+import { withLogoutRedirect } from '../../hoc/withAuthRedirect'
 
 const Profile = (props) => {
     return (
@@ -21,10 +19,5 @@ const Profile = (props) => {
         </div>
     )
 }
-const mapStateToProps = (state) => ({
-    userId: state.auth.userId,
-    isAuth: state.auth.isAuth,
-    isAdmin: state.auth.isAdmin,
-})
 
-export default compose(connect(mapStateToProps), withLoginRedirect)(Profile)
+export default withLoginRedirect(Profile)
