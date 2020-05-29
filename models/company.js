684b3d897a51ba0813e246a704bab7caaa347cb4
Vadim Safronov <vadimisprice@tut.by>
2020-05-29 21:26:05 +0300

    create company

---
+++ models/company.js
@@ -0,0 +1,35 @@
+odule.exports = (sequelize, DataTypes) => {
+    const Company = sequelize.define('company', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        videoLink: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        currentAmount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        targetAmount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        expirationDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+    })
+
+    Company.associate = (models) => {
+        Company.belongsTo(models.users)
+        Company.hasMany(models.bonuses)
+    }
+
+    return Company
+}
