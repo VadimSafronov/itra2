941b60ff8d90e95370141e1033b8a0dc9d2e8062
Vadim Safronov <vadimisprice@tut.by>
2020-05-29 17:51:11 +0300

    locale

--- coursework2/src/components/Admin/parts/TableToolBar.js
+++ coursework2/src/components/Admin/parts/TableToolBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
-import translate from '../../localizations/translate'
+import translate from '../../../localizations/translate'
 import { connect } from 'react-redux'
 
 import Toolbar from '@material-ui/core/Toolbar'
