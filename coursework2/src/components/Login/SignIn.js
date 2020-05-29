e6b7d458fb461fe7b506a449e4979a3b18ff27ab
Vadim Safronov <vadimisprice@tut.by>
2020-05-29 14:12:21 +0300

    fix buttons

--- coursework2/src/components/Login/SignIn.js
+++ coursework2/src/components/Login/SignIn.js
@@ -21,8 +21,8 @@ import Snackbar from '@material-ui/core/Snackbar'
 import { ErrorAlert } from '../../validation/ErrorAlert'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
-// import GoogleAuth from "./GoogleAuth";
-// import FacebookAuth from './FacebookAuth'
+import GoogleAuth from "./GoogleAuth";
+import FacebookAuth from './FacebookAuth';
 
 
 
@@ -74,10 +74,10 @@ const SignIn = (props) => {
         <Typography component="h1" variant="h5">
         {translate('sign.logo')}
         </Typography>
-        {/* <div className={classes.socialGroup}>
+        <div className={classes.socialGroup}>
           <GoogleAuth />
           <FacebookAuth />
-        </div> */}
+        </div>
         <form className={classes.form} noValidate>
           <Field
             variant="outlined"
