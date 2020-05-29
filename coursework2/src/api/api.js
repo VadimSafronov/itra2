ab4047ed6f75561ed9db5d161c8b380fb16e89fd
Vadim Safronov <vadimisprice@tut.by>
2020-05-29 22:58:35 +0300

    update api

--- coursework2/src/api/api.js
+++ coursework2/src/api/api.js
@@ -11,8 +11,8 @@ export const authAPI = {
     registration(login, password) {
         return instance.post('auth/registration', { login, password })
     },
-    socialLogin(Id, name) {
-        return instance.post('auth/socialLogin', {Id, name })
+    socialLogin(socialId, name) {
+        return instance.post('auth/socialLogin', { socialId, name })
     },
     me() {
         return instance.post('auth/me')
@@ -22,23 +22,6 @@ export const authAPI = {
     },
 }
 
-export const profileAPI = {
-    getBonusesCount(userId) {
-        return instance.post('profile/bonuses/count', { userId })
-    },
-    getBonuses(userId, offset, limit) {
-        return instance.post('profile/bonuses/get', { userId, offset, limit })
-    },
-    getCompanyCount(userId) {
-        return instance.post('profile/company/count', { userId })
-    },
-    getCompany(userId, offset, limit) {
-        return instance.post('profile/company/get', { userId, offset, limit })
-    },
-    getUserInfo(userId) {
-        return instance.post('profile/user/get', { userId })
-    },
-}
 export const adminAPI = {
     getUsersCount() {
         return instance.get('admin/users/count')
@@ -63,3 +46,63 @@ export const adminAPI = {
     },
 }
 
+export const profileAPI = {
+    getBonusesCount(userId) {
+        return instance.post('profile/bonuses/count', { userId })
+    },
+    getBonuses(userId, offset, limit) {
+        return instance.post('profile/bonuses/get', { userId, offset, limit })
+    },
+    getCompanyCount(userId) {
+        return instance.post('profile/company/count', { userId })
+    },
+    getCompany(userId, offset, limit) {
+        return instance.post('profile/company/get', { userId, offset, limit })
+    },
+    getUserInfo(userId) {
+        return instance.post('profile/user/get', { userId })
+    },
+}
+
+export const companyAPI = {
+    getUserCompanies(userId) {
+        return instance.post('company/userCompanies/getId', { userId })
+    },
+    getCompany(companyId) {
+        return instance.post('company/company/get', { companyId })
+    },
+    getBonusesCount(companyId) {
+        return instance.post('company/bonuses/count', { companyId })
+    },
+    getBonuses(companyId, offset, limit) {
+        return instance.post('company/bonuses/get', { companyId, offset, limit })
+    },
+    buyBonus(bonusId, userId, companyId, bonusAmount) {
+        return instance.post('company/bonuses/buy', { bonusId, userId, companyId, bonusAmount })
+    },
+    createCompany(userId, title, description, videoLink, targetAmount, expirationDate) {
+        return instance.post('company/create', {
+            userId,
+            title,
+            description,
+            videoLink,
+            targetAmount,
+            expirationDate,
+        })
+    },
+}
+
+export const homeAPI = {
+    getCompany() {
+        return instance.post('home/company/get')
+    },
+}
+
+export const bonusAPI = {
+    getCompaniesId(userId) {
+        return instance.post('bonus/companiesId/get', { userId })
+    },
+    createBonus(title, amount, description, companyId) {
+        return instance.post('bonus/create', { title, amount, description, companyId })
+    },
+}
