32555cb6ba416ed03b86a89b2a4bf02dcdd39dfe
Vadim Safronov <vadimisprice@tut.by>
2020-05-30 21:13:45 +0300

    add bonuses

---
+++ coursework2/src/components/CreateBonus/CreateBonusForm.js
@@ -0,0 +1,104 @@
+import React, { useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import translate from '../../localizations/translate'
+import { Field, reduxForm } from 'redux-form'
+import { renderField } from '../../validation/Fields'
+import Button from '@material-ui/core/Button'
+import { required, maxLengthCreator } from '../../validators/index'
+import MenuItem from '@material-ui/core/MenuItem'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { InfoAlert } from '../../validation/InfoAlert'
+import { renderSelectField } from '../../validation/Fields'
+import { toggleStatus } from '../../Redux/bonusReducer'
+
+const useStyles = makeStyles((theme) => ({
+    createForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        '& > *': {
+            marginBottom: 20,
+        },
+        '& > *:last-child': {
+            marginTop: 5,
+            marginBottom: 0,
+        },
+    },
+}))
+
+const maxLength20 = maxLengthCreator(20)
+const maxLength40 = maxLengthCreator(40)
+
+const CreateBonusForm = (props) => {
+    const classes = useStyles()
+
+    return (
+        <form onSubmit={props.handleSubmit} className={classes.createForm}>
+            <Field
+                name='companyId'
+                component={renderSelectField}
+                label={translate('bonusCreate.selectCompany')}
+                validate={[required]}
+            >
+                {props.companiesId.map((option) => (
+                    <MenuItem key={option.id} value={option.id}>
+                        {option.title}
+                    </MenuItem>
+                ))}
+            </Field>
+
+            <Field
+                name='title'
+                autoComplete='off'
+                component={renderField}
+                label={translate('bonusCreate.inputTitle')}
+                validate={[required, maxLength20]}
+            />
+
+            <Field
+                name='amount'
+                type='number'
+                autoComplete='off'
+                component={renderField}
+                label={translate('bonusCreate.inputAmount')}
+                validate={[required, maxLength20]}
+            />
+
+            <Field
+                name='description'
+                autoComplete='off'
+                component={renderField}
+                label={translate('bonusCreate.inputDescription')}
+                validate={[required, maxLength40]}
+            />
+
+            <Button
+                type='submit'
+                variant='contained'
+                color='primary'
+                size='large'
+                disabled={props.isFetching}
+            >
+                {translate('bonusCreate.create')}
+            </Button>
+
+            <InfoAlert
+                open={!!props.statusCode}
+                message={translate(`bonusCreate.message${props.statusCode}`)}
+                severity={props.statusCode === 200 ? 'success' : 'error'}
+                onClose={props.toggleStatus}
+            />
+        </form>
+    )
+}
+
+const mapStateToProps = (state) => ({
+    companiesId: state.bonus.companiesId,
+    isFetching: state.bonus.isFetching,
+    statusCode: state.bonus.statusCode,
+})
+
+export default compose(
+    connect(mapStateToProps, { toggleStatus }),
+    reduxForm({ form: 'bonus' })
+)(CreateBonusForm)
