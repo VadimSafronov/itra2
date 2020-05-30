4274cfbcc8632a917f48dc10f989ccb1637be45a
Vadim Safronov <vadimisprice@tut.by>
2020-05-30 22:05:03 +0300

    add validator

--- coursework2/src/validation/Fields.js
+++ coursework2/src/validation/Fields.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField'
+import React from "react";
+import TextField from "@material-ui/core/TextField";
 
-export const renderField = ({ label, input, meta: { touched, invalid, error }, ...custom }) => (
-    <TextField
-        label={label}
-        error={touched && invalid}
-        helperText={touched && error}
-        variant='outlined'
-        {...input}
-        {...custom}
-    />
-
-    
-)
+export const renderField = ({
+  label,
+  input,
+  meta: { touched, invalid, error },
+  ...custom
+}) => (
+  <TextField
+    label={label}
+    error={touched && invalid}
+    helperText={touched && error}
+    variant="outlined"
+    {...input}
+    {...custom}
+  />
+);
 export const renderSelectField = ({
     input,
     label,
